9e00b30187127ed86b7e34e881f7559bbd0b346d
Ануар Джусупов <anyap.tape@outlook.com>
2017-05-18 01:51:36 +0600

    PreFinal

---
+++ Task1_WorkWithFile/Task1_WorkWithFile/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+
+namespace Task1_WorkWithFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream file = new FileStream(@"C:\final\task1.txt", FileMode.Create);
+            StreamWriter sw = new StreamWriter(file);
+            sw.WriteLine("1235 1455 1898 1423");
+            sw.Write("5 9 3 0");
+            sw.Close();
+            file.Close();
+
+            //считывания содержимое как string и через сплит заносим в массив типа string
+            string text = File.ReadAllText(@"C:\final\task1.txt");
+            Console.WriteLine("Файл lab2.txt создан\n\nСодержимое файла:\n{0}", text);
+            string[] temp = text.Split(' ', '\n');
+
+            //почитал документацию, нашел как тут пользоваться листом
+            List<int> numbers = new List<int>();  //все числа
+            List<int> prime = new List<int>();   //простые числа
+
+
+            for (int i = 0; i < temp.Length; i++)
+            {
+                int tmp = 0;
+                numbers.Add(int.Parse(temp[i])); //считываем все числа
+                //ищем простые и заносим в лист простых
+                for (int k = 1; k <= int.Parse(temp[i]); k++)
+                    if (int.Parse(temp[i]) % k == 0)
+                        tmp++;
+
+                if (tmp == 2) prime.Add(int.Parse(temp[i]));
+            }
+            //сортируем, чтобы вывести макс и мин
+            numbers.Sort();
+            prime.Sort();
+
+            Console.WriteLine("\n\nMAX NUMBER: {1}, MIN MAX NUMBER: {0}", numbers[0], numbers[numbers.Count() - 1]);
+            Console.WriteLine("\n\nMAX PRIME: {1}, MIN PRIME: {0}", prime[0], prime[prime.Count() - 1]);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task2_Delegate/Task2_Delegate/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_Delegate
+{
+    public delegate string PrintDelegate(string s, int b);
+    class Sample
+    {
+        public string MyFunction(string s, int b)
+        {
+            string str = "";
+            for (int i = 0; i < b; i++)
+                str += s;
+            return str;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sample h = new Sample();
+            PrintDelegate d = h.MyFunction;
+            Console.WriteLine(d.Invoke("Test", 10));
+            Console.ReadKey();
+        }
+    }
+}
